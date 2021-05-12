f914c8109708566f4cf17430db4254a9f2facecc
Joshmerm <joshmerm@gmail.com>
2021-05-11 22:04:34 -0700

    Update to calories and home

--- Components/Calories.js
+++ Components/Calories.js
@@ -20,9 +20,11 @@ const Calories = (props) => {
     );
 
     const [caloriesForMeal, setCaloriesForMeal] = useState(20);
+    const [shadowRadius, setShadowRadius] = useState(0);
+    const [shadowOpacity, setShadowOpacity] = useState(0.1);
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{margin: 20, height: 50, width: '60%', borderRadius: 7, alignSelf: 'center', justifyContent: 'center', backgroundColor: '#0096FF'}}>
+            <View style={[{margin: 20, height: 50, width: '60%', borderRadius: 7, alignSelf: 'center', justifyContent: 'center', backgroundColor: '#0096FF', shadowColor: '#00000', shadowOffset: {width: 4, height: 4}, shadowOpacity: 0.4, shadowRadius: 7, elevation: 10}]}>
                 <Text style={styles.caloriesConsumed}>Calories Consumed: {props.calories}</Text>
             </View>
             <TouchableOpacity style={styles.addNewWorkout} onPress={() => props.navigation.navigate("AddMeal")}>
@@ -68,6 +70,9 @@ const styles = StyleSheet.create({
     caloriesConsumed: {
         margin: 10,
         alignSelf: 'center',
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold'
     },  
     workout: {
         alignSelf: 'center',
@@ -91,7 +96,9 @@ const styles = StyleSheet.create({
         color: 'white'
     },
     item: {
-        backgroundColor: '#f9c2ff',
+        width: '80%',
+        alignSelf: 'center',
+        backgroundColor: '#B0C4DE',
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
@@ -100,11 +107,29 @@ const styles = StyleSheet.create({
         fontSize: 32,
     },
     addNewWorkout: {
-        width: "80%",
+        width: "60%",
         alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'red',
-        height: 100
+        backgroundColor: '#FA8072',
+        borderColor: '#eb6959',
+        borderWidth: 3,
+        borderRadius: 10,
+        height: 75,
+        shadowColor: '#000000', 
+        shadowOffset: {width: 4, height: 4}, 
+        shadowOpacity: 0.7, 
+        shadowRadius: 7, 
+        elevation: 10,
+        marginBottom: 20,
+    },
+    name: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    duration: {
+        color: 'white',
+        fontWeight: '600'
     }
 });
--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,35 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const Home = (props) => {
     const [caloriesForMeal, setCaloriesForMeal] = useState(20);
     return (
         <View style={styles.container}>
+            <View style={[{marginTop: 20, height: 50, width: '60%', borderRadius: 0, alignSelf: 'center', justifyContent: 'center', backgroundColor: '#0096FF', shadowColor: '#00000', shadowOffset: {width: 4, height: 4}, shadowOpacity: 0.4, shadowRadius: 7, elevation: 10}]}>
+                <Text>Total Calories Eaten: {props.calories}</Text>
+            </View>
+            <View style={[{ height: 50, width: '60%', borderRadius: 0, alignSelf: 'center', justifyContent: 'center', backgroundColor: '#0096FF', shadowColor: '#00000', shadowOffset: {width: 4, height: 4}, shadowOpacity: 0.4, shadowRadius: 7, elevation: 10}]}>
+                <Text>Total Calories Burned: {props.caloriesBurned}</Text>
+            </View>
+            <View style={[{marginBottom: 20, height: 50, width: '60%', borderRadius: 0, alignSelf: 'center', justifyContent: 'center', backgroundColor: '#0096FF', shadowColor: '#00000', shadowOffset: {width: 4, height: 4}, shadowOpacity: 0.4, shadowRadius: 7, elevation: 10}]}>
+            <Text>Total Calories: {props.calories - props.caloriesBurned} </Text>
+            </View>
             <TouchableOpacity style={styles.workout}
                 onPress={() => props.navigation.navigate("WorkoutScreen")}
-                >
+            >
                 <Text style={styles.text}>WorkOut</Text>
             </TouchableOpacity>
-            <TouchableOpacity 
+            <TouchableOpacity
                 style={styles.calories}
                 onPress={() => props.navigation.navigate("Calories")}
-                >
+            >
                 <Text style={styles.text}>Calories</Text>
             </TouchableOpacity>
-            <Text>Total Calories Eaten: {props.calories}</Text>
-            <Text>Total Calories Burned: {props.caloriesBurned}</Text>
-            <Text>Total Calories: {props.calories - props.caloriesBurned} </Text>
-            
+
             <StatusBar style="auto" />
         </View>
     );
 }
 
-function mapStateToProps(state){
-    return{
+function mapStateToProps(state) {
+    return {
         calories: state.calories,
         caloriesBurned: state.caloriesBurned
     }
@@ -61,13 +67,13 @@ const styles = StyleSheet.create({
         height: 200,
         backgroundColor: 'red',
     },
-    calories:{
+    calories: {
         alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'center',
         width: '80%',
         height: 200,
-        backgroundColor: 'green', 
+        backgroundColor: 'green',
     },
     text: {
         fontWeight: '500',
